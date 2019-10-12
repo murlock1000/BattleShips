06f9f6502ba60a9f20c83fa37361a687d2a1b173
geigosam <56157793+geigosam@users.noreply.github.com>
2019-10-12 19:35:45 +0300

    Fixed odd indentation issues in backend.cpp file

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -18,85 +18,85 @@ void write (int buffer) {
 }
 
 int main () {
-	//these variables might become changeable from frontend later
-	
-	int tableWidth = 10;
-	int tableHeight = 10;
-	int shipNumber = 5;
-	int playerNumber = 2;
+    //these variables might become changeable from frontend later
+    
+    int tableWidth = 10;
+    int tableHeight = 10;
+    int shipNumber = 5;
+    int playerNumber = 2;
 
-	//initialisation
-	
+    //initialisation
+    
     int shipHealth [playerNumber] [shipNumber];
-	int shipTable [playerNumber] [tableWidth * tableHeight];
-	int shipsLeft [playerNumber];
+    int shipTable [playerNumber] [tableWidth * tableHeight];
+    int shipsLeft [playerNumber];
 
-	for (int i = 0; i < playerNumber; i++) {
+    for (int i = 0; i < playerNumber; i++) {
         shipsLeft [i] = shipNumber;
 
         for (int j=0; j < shipNumber; j++) {
             shipHealth [i] [j] = 0;
         }
 
-		for (int j = 0; j < tableWidth * tableHeight; j++) {
+        for (int j = 0; j < tableWidth * tableHeight; j++) {
             shipTable [i] [j] = read (); //read function
             
             if (shipTable [i] [j] != 0) {
                 shipHealth [i] [shipTable [i] [j] - 1] ++;
             } 
 
-		}
-	}
+        }
+    }
 
-	int currentPlayer = 0; //player 1 starts
-	int opponentPlayer = 1; //playing against the next player
+    int currentPlayer = 0; //player 1 starts
+    int opponentPlayer = 1; //playing against the next player
 
-	//main game
+    //main game
 
-	while (true) {
-		int tileX;
-		int tileY;
+    while (true) {
+        int tileX;
+        int tileY;
 
-		tileX = read (); //read function
+        tileX = read (); //read function
         tileY = read (); //read function
 
         tileX--;
         tileY--;
 
-		if (shipTable [opponentPlayer] [tableWidth * tileY + tileX] == 0) {
+        if (shipTable [opponentPlayer] [tableWidth * tileY + tileX] == 0) {
 
-			write (-1); //write function
+            write (-1); //write function
 
-		}
+        }
 
-		else {
+        else {
             
-			if (--shipHealth [opponentPlayer] [shipTable [opponentPlayer] [tableWidth * tileY + tileX] - 1] == 0) {
-				shipsLeft [opponentPlayer]--;
+            if (--shipHealth [opponentPlayer] [shipTable [opponentPlayer] [tableWidth * tileY + tileX] - 1] == 0) {
+                shipsLeft [opponentPlayer]--;
 
-				/*write (2); //write function
+                /*write (2); //write function
                 write (shipTable [opponentPlayer] [tableWidth * tileY + tileX]); //write function*/
-			       	
-			}
+                    
+            }
 
-			else {
+            else {
 
-				//write (1); //write function
+                //write (1); //write function
 
-			}
+            }
 
             write (1);
 
-		}
+        }
 
-		if (shipsLeft [opponentPlayer] == 0) {
-			break;
-		}
+        if (shipsLeft [opponentPlayer] == 0) {
+            break;
+        }
 
-		currentPlayer = (currentPlayer + 1) % playerNumber;
-        opponentPlayer = (currentPlayer + 1) % playerNumber;	
+        currentPlayer = (currentPlayer + 1) % playerNumber;
+        opponentPlayer = (currentPlayer + 1) % playerNumber;    
 
-	}
+    }
     
-	return 0;
+    return 0;
 }

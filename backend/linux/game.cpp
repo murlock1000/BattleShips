967f862e3c4221a4b34df73346dc3ede879e427b
murlock1000 <pauliusjankauskas10@gmail.com>
2020-02-17 18:57:02 +0100

    HOTFIX for linux game.cpp

--- backend/linux/game.cpp
+++ backend/linux/game.cpp
@@ -186,6 +186,8 @@ stringstream ss;
 			pid [i] = *aiPid;
 			fdInput [i] = aiIO [0]; //Store each player's fds
 			fdOutput [i] = aiIO [1];
+	
+			stdRead (fdInput [i]); //read a 0 from the AI
 		}
 
 		//generating map string
